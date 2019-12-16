1dffc0183d10a38904c53132ff4123de0df1b359
Nathan Persall <npersall@gmail.com>
2019-12-15 23:24:15 -0800

    initial commit

---
+++ calculator.js
@@ -0,0 +1,13 @@
+class Calculator {
+  constructor() {
+    this.displayValue = '0';
+    this.Operand = null;
+    this.waitingForSecondOperand = false;
+    operator = null;
+  }
+  function updateDisplay() {
+    const display = document.querySelector('./calculaor-screen')
+
+
+  }
+};
