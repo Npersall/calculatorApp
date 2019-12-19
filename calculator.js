88832c963a72baed3d01b6f84fc764d1fc41aa36
Nathan Persall <npersall@gmail.com>
2019-12-19 15:37:25 -0800

    completes logic for displaying value on screen

--- calculator.js
+++ calculator.js
@@ -1,40 +1,46 @@
+'Use Strict';
+
 class Calculator {
-  constructor() {
-    this.displayValue = '0';
-    this.Operand = null;
-    this.waitingForSecondOperand = false;
-    operator = null;
+  constructor(displayValue) {
+  this.displayValue = displayValue;
+  this.firstOperand =  null;
+  this.waitingForSecondOperand = false;
+  this.operator= null;
+  }
+   updateDisplay(value) {
+     const display = document.querySelector(".calculator-screen");
+    this.displayValue = value;
+    display.value = this.displayValue;
   }
 }
 
-  function  updateDisplay() {
-    const display = document.querySelector('.calculator-screen');
-    display.value = calculator.displayValue;
-  }
+const calculator = new Calculator();
+console.log(calculator);
+calculator.updateDisplay(0);
 
-  updateDisplay();
 
-  const keys = document.querySelector('.calculator-keys');
-  keys.addEventListener(`click`, (event) => {
-    const{ target } = event;
-    if (!target.matches('button')) {
-      return;
-    }
 
-  if (target.classList.contains('operator')) {
-    console.log('operator', target.value);
-    return;
-  }
+//   const keys = document.querySelector('.calculator-keys');
+//   keys.addEventListener(`click`, (event) => {
+//     const{ target } = event;
+//     if (!target.matches('button')) {
+//       return;
+//     }
 
-  if (target.classList.contains('decimal')) {
-    console.log('decimal', target.value);
-    return;
-  }
-if (target.classList.contains('all-clear')) {
-  console.log('clear', target.value);
-  return;
-}
+//   if (target.classList.contains('operator')) {
+//     console.log('operator', target.value);
+//     return;
+//   }
 
-console.log('digit', target.value);
+//   if (target.classList.contains('decimal')) {
+//     console.log('decimal', target.value);
+//     return;
+//   }
+// if (target.classList.contains('all-clear')) {
+//   console.log('clear', target.value);
+//   return;
+// }
 
-});
+// console.log('digit', target.value);
+
+
